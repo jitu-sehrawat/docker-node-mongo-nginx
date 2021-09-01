88293f592a6cd0248a0e7c9089442210752d34a4
Jitender Sehrawat <jitu.sehrawat@gmail.com>
2021-09-01 22:36:57 +0530

    Basic CRUD

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const morgan = require('morgan')
 const mongoose = require('mongoose');
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require('./config/config')
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require('./config/config');
+const postRouter = require('./routes/postRoutes');
 
 const app = express();
 
+app.use(morgan('combined'));
+app.use(express.json());
 // here "mongo" is the name of Mongodb Service name compose file
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 const connectWithRetry = () => {
@@ -21,6 +25,7 @@ connectWithRetry();
 app.get(`/`, (req, res) => {
   res.send(`<h2>Hi There !!!<h2/> ${process.env.NODE_ENV}`)
 });
+app.use('/api/v1/posts', postRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
