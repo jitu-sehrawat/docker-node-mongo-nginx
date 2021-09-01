eade5396b05ff9a8d97e3aa86b16b508a95f12ae
Jitender Sehrawat <jitu.sehrawat@gmail.com>
2021-09-01 18:42:14 +0530

    Added Mongodb, attached a name voulme

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+// here "mongo" is the name of Mongodb Service name compose file
+mongoose.connect('mongodb://root:Cardekho123@mongo:27017/?authSource=admin')
+  .then(() => console.log("Connected to DB"))
+  .catch((e) => console.log(e));
 
 app.get(`/`, (req, res) => {
   res.send(`<h2>Hi There !!!<h2/> ${process.env.NODE_ENV}`)
