a5e16595581648b6e184b8f468ce1d918cb31468
Jitender Sehrawat <jitu.sehrawat@gmail.com>
2021-09-03 01:33:57 +0530

    add auth session redis

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const morgan = require('morgan')
 const mongoose = require('mongoose');
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require('./config/config');
+const redis = require('redis');
+const session = require('express-session');
+let RedisStore = require('connect-redis')(session)
+
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_URL, SESSION_SECRET, REDIS_PORT} = require('./config/config');
 const postRouter = require('./routes/postRoutes');
+const userRouter = require('./routes/userRoute');
+
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT
+});
 
 const app = express();
 
-app.use(morgan('combined'));
-app.use(express.json());
 // here "mongo" is the name of Mongodb Service name compose file
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 const connectWithRetry = () => {
@@ -22,10 +30,27 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      saveUninitialized: false,
+      resave: false,
+      secure: false,
+      httpOnly: true,
+      maxAge: 300000000
+    }
+  })
+)
+app.use(morgan('combined'));
+app.use(express.json());
+
 app.get(`/`, (req, res) => {
   res.send(`<h2>Hi There !!!<h2/> ${process.env.NODE_ENV}`)
 });
 app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/users', userRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 
 const postController = require('../controllers/postController');
+const protect = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 //  localhost:3000/
 router
   .route('/')
-  .get(postController.getAllPosts)
-  .post(postController.createPost);
+  .get(protect, postController.getAllPosts)
+  .post(protect, postController.createPost);
 
 // localhost:3000/:id
 router
   .route('/:id')
-  .get(postController.getOnePost)
-  .patch(postController.updatePost)
-  .delete(postController.deletePost);
+  .get(protect, postController.getOnePost)
+  .patch(protect, postController.updatePost)
+  .delete(protect, postController.deletePost);
 
 module.exports = router;
